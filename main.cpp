c114018f9ed394a3430674512ff4dd013d533120
Bartłomiej Zalas <bartekzalas@gmail.com>
2023-08-13 13:23:46 +0200

    basic interaction with the program provided by main

--- main.cpp
+++ main.cpp
@@ -6,25 +6,47 @@
 
 using namespace std;
 
+void getUserInput(string &input)
+{
+    cout << "Type text to encode (\"exit\" will close the program) " << endl;
+    cin >> input;
+}
+
+bool userInpupIsValid(const string &input)
+{
+    for (char l : input)
+    {
+        if (charToInt(l) < 0 || charToInt(l) > 25)
+        {
+            cout << "[ERROR] Input contains incorrect not encodable character: '" << l << "'." << endl;
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
-    cout << "Enigma Machine" << endl;
-
     auto rotorI = make_unique<Rotor>("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'A');
     auto rotorII = make_unique<Rotor>("AJDKSIRUXBLHWTMCQGZNPYFVOE", 'A');
     auto rotorIII = make_unique<Rotor>("BDFHJLCPRTXVZNYEIWGAKMUSQO", 'A');
-
-    vector<pair<char, char>> pairs = {};
+    auto pairs = {make_pair('A', 'B')};
     auto plugboard = make_unique<Plugboard>(pairs);
-
     auto ukwB = make_unique<Reflector>("YRUHQSLDPXNGOKMIEBFZCWVJAT");
-
     Enigma enigma(rotorI, rotorII, rotorIII, plugboard, ukwB);
 
-    char toEncode = 'A';
-    char encoded = enigma.encode('A');
+    cout << "Enigma Machine" << endl;
 
-    cout << "Letter '" << toEncode << "' encoded to '" << encoded << "'." << endl;
+    string input;
+    getUserInput(input);
+
+    while (input != "exit" && userInpupIsValid(input))
+    {
+        string encoded = enigma.encode(input);
+        cout << "Text to encode: " << input << endl;
+        cout << "Encoded text: " << encoded << endl;
+        getUserInput(input);
+    };
 
     return 0;
 }
